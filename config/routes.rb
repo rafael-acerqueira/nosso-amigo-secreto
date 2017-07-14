b19b73b0dff9f75ce8e5a3e2a8de7f3f190579cb
Rafael Aquino <rafael.acerqueirah@gmail.com>
2017-07-14 15:52:05 -0300

    Adjust routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,16 @@
 require 'sidekiq/web'
 
 Rails.application.routes.draw do
-  get 'members/create'
-
-  get 'members/destroy'
-
-  get 'members/update'
-
-  get 'campaigns/show'
-
-  get 'campaigns/index'
-
-  get 'campaigns/create'
-
-  get 'campaigns/update'
-
-  get 'campaigns/destroy'
-
-  get 'campaigns/raffle'
-
-  get 'pages/home'
 
   devise_for :users, :controllers => { registrations: 'registrations' }
   mount Sidekiq::Web => '/sidekiq'
+
+  get 'members/:token/opened', to: 'member#opened'
+  resources :members, only: [:create, :destroy, :update]
+
+  resources :campaigns, execpt: [:new] do
+    post 'raffle', on: :member
+  end
+
+  root_to: 'pages#home'
 end
