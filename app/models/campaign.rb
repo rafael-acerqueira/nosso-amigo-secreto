5012022ca0ef0738b97bb11092219c1c6af5ce5b
Rafael Aquino <rafael.acerqueirah@gmail.com>
2017-07-22 10:58:52 -0300

    Update models

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,3 +1,16 @@
 class Campaign < ApplicationRecord
   belongs_to :user
+  has_many :members, dependent: :destroy
+  before_create :set_member
+  before_create :set_status
+  enum status: [:pending, :finished]
+  validates :title, :description, :user, :status, presence: true
+
+  def set_status
+    self.status = :pending
+  end
+
+  def set_member
+    self.members << Member.create(name: self.user.name, email: self.user.email)
+  end
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,3 +1,20 @@
 class Member < ApplicationRecord
   belongs_to :campaign
+  after_save :set_campaign_pending
+  validates :name, :email, :campaign, presence: true
+
+  def set_pixel
+    self.open = false
+    self.token = loop do
+      random_token = SecureRandom.urlsafe_base64(nil, false)
+      break random_token unless Member.exists?(token: random_token)
+    end
+    self.save!
+  end
+
+  protected
+
+  def set_campaign_pending
+    self.campaign.update(status: :pending)
+  end
 end
