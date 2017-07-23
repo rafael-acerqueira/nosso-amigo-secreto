88b985388aee1ee794993d378a9921fa0fdd2074
Rafael Aquino <rafael.acerqueirah@gmail.com>
2017-07-23 10:19:30 -0300

    Add customize rescue from 404 error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
   protect_from_forgery with: :exception
+
+  rescue_from ActiveRecord::RecordNotFound, :with => :render_404
+
+  def render_404
+    redirect_to main_app.root_url
+  end  
 end
