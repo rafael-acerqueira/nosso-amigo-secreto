8fd519d1d95be419e34f8c93d7822fc299cc8f90
Rafael Aquino <rafael.acerqueirah@gmail.com>
2017-07-23 14:06:52 -0300

    Update MemberController and Create MemberControllerSpec

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,10 +1,65 @@
 class MembersController < ApplicationController
+  before_action :authenticate_user!, except: [:opened]
+
+  before_action :set_member, only: [:show, :destroy, :update]
+  before_action :is_owner?, only: [:destroy, :update]
+  before_action :set_member_by_token, only: [:opened]
+
   def create
+    @member = Member.new(member_params)
+
+    respond_to do |format|
+      if @member.save
+        format.json { render json: true }
+      else
+        format.json { render json: @member.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+    @member.destroy
+
+    respond_to do |format|
+      format.json { render json: true }
+    end
   end
 
   def update
+    respond_to do |format|
+      if @member.update(member_params)
+        format.json { render json: true }
+      else
+        format.json { render json: @member.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def opened
+    @member.update(open: true)
+    gif = Base64.decode64("R0lGODlhAQABAPAAAAAAAAAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==")
+    render text: gif, type: 'image/gif'
+  end
+
+  private
+
+  def set_member
+    @member = Member.find(params[:id])
+  end
+
+  def set_member_by_token
+    @member = Member.find_by!(token: params[:token])
+  end
+
+  def member_params
+    params.require(:member).permit(:name, :email, :campaign_id)
+  end
+
+  def is_owner?
+    unless current_user == @member.campaign.user
+      respond_to do |format|
+        format.json { render json: false, status: :forbidden }
+      end
+    end
   end
 end
--- spec/controllers/members_controller_spec.rb
+++ spec/controllers/members_controller_spec.rb
@@ -1,26 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe MembersController, type: :controller do
+  include Devise::Test::ControllerHelpers
 
-  describe "GET #create" do
+  before(:each) do
+    request.env["HTTP_ACCEPT"] = 'application/json'
+    @request.env["devise.mapping"] = Devise.mappings[:user]
+    @current_user = FactoryGirl.create(:user)
+    sign_in @current_user
+  end
+
+  describe "POST #create" do
+
     it "returns http success" do
-      get :create
+      campaign = create(:campaign, user: @current_user)
+      member_attributes = attributes_for(:member).merge(campaign_id: campaign.id)
+      post :create, params: {member: member_attributes}
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET #destroy" do
+  describe "DELETE #destroy" do
     it "returns http success" do
-      get :destroy
+      campaign = create(:campaign, user: @current_user)
+      member = create(:member, campaign: campaign)
+      delete :destroy, params: {id: member.id}
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET #update" do
+  describe "PUT #update" do
     it "returns http success" do
-      get :update
+      member_attributes = attributes_for(:member)
+      campaign = create(:campaign, user: @current_user)
+      member = create(:member, campaign: campaign)
+      put :update, params: {id: member.id, member: member_attributes}
       expect(response).to have_http_status(:success)
     end
   end
-
 end
